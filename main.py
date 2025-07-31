32282cdb923198cec92a67c552080a7c019f3ee2
mirudhula1027 <csbs269038@saranathan.ac.in>
2025-08-01 00:36:21 +0530

    main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ def hello():
     return 'Hello! This is a simple Python app on Google App Engine.'
 
 if _name_ == '_main_':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
