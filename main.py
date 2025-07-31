2c37f2f603d64aa335e4f01224f072042c395819
mirudhula1027 <csbs269038@saranathan.ac.in>
2025-08-01 00:45:13 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(_name_)
 
 @app.route('/')
 def hello():
-    return 'Hello! This is a simple Python app on Google App Engine.'
+    return 'Hello, World!'
 
 if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
