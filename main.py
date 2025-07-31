69bcfae54e0e7e2af4b0c8b525d0b0b042464cbb
mirudhula1027 <csbs269038@saranathan.ac.in>
2025-08-01 01:22:46 +0530

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "✅ Hello from Cloud Shell on port 8080!"
+    return "Hello from Cloud Shell on port 8080!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
